ec909a9ecd5248ec6ec1e0f995279fb11c7f1380
MrcCravo <ferramentas@maracs.com.br>
2023-10-02 22:42:33 -0300

    mais um dia e um novo curso, mantendo a consistencia, commit everyday

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Boas vindas ao jogo do número secreto!"); 
+let numeroSecreto = 5;
+let chute = prompt("Chute um número para advinhar o número secreto entre 1 e 30");
+
+if (numeroSecreto == chute) {
+    console.log("Isso aí! Você descobriu o número secreto (5) ");
+}
